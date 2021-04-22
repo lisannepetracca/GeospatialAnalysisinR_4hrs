2acb52c40e12de135cd4471e468aacffca0dff40
Amanda Cheeseman <acheesem@syr.edu>
2021-04-21 21:48:56 -0400

    updated pre workshop steps

--- PreWorkshop_Steps.R
+++ PreWorkshop_Steps.R
@@ -1,16 +1,10 @@
 #Let's run through some steps to make sure we are good to go for the workshop
 #First, let's install some packages
 
-#We will install "devtools" first to get two packages that are not in the CRAN library
-#***PLEASE HIT ENTER WHEN IT ASKS WHAT UPDATES YOU WANT FOR THE SECOND PACKAGE***
-install.packages("devtools")
-devtools::install_version("velox", version = "0.2.0")
-devtools::install_github("ropensci/FedData")
-
 #then we will install other packages that *are* in the CRAN library
 #***IF IT ASKS TO RESTART R, YOU CAN SAY YES***
-install.packages(c("sp", "sf", "raster", "rgeos", "ggplot2", "dplyr", "units", "rnaturalearth", "rgdal", "ggmap",
-                   "move","adehabitatHR","rgbif", "grid"))
+install.packages(c("sp", "sf", "raster", "rgeos", "ggplot2", "dplyr", "units", "rnaturalearth", 
+                   "rgbif"))
 #***RED TEXT DOES NOT MEAN IT DIDN'T WORK. THERE IS ONLY AN ISSUE IF YOU SEE SOMETHING LIKE "Error in install.packages>"***
 
 #now let's get these packages loaded into R
@@ -22,14 +16,9 @@ library(ggplot2)
 library(dplyr)
 library(units)
 library(rnaturalearth)
-library(rgdal)
-library(ggmap)
-library(move)
-library(adehabitatHR)
 library(rgbif)
-library(grid)
-library(velox)
-library(FedData)
+
+
 #we are ALL GOOD on packages if you do not get any error messages after running these "library" lines
 
 #now we will set our working directory
